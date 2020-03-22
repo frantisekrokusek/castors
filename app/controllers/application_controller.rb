3d3525346ddde36a9d8cbc19b8718e3376f6d5e6
Frantisek <frantisek.rokusek@gmail.com>
2020-03-22 13:38:56 +0100

    create main layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,18 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   before_action :authenticate_user!
 
+  layout :layout_by_resource
+
   def home
+  end
+
+  private
+
+  def layout_by_resource
+    if user_signed_in?
+      "application"
+    else
+      "application_unauthorized"
+    end
   end
 end
